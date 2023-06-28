f45998b56f07b55d04e4d1c4de57e283dc7d8693
Pedro Zoia <pedro.mauro.pb@compasso.com.br>
2023-06-28 20:12:09 -0300

    imagens atualizadas

--- data.js
+++ data.js
@@ -2,19 +2,19 @@ var posts = [
     {
         id: 1,
         title: 'Hollowknight',
-        imageUrl: 'img/hollowknight.png',
+        imageUrl: 'img/hollowknight.jpg',
         body: 'The protagonist of Hollow Knight, known as "The Knight", is a silent and enigmatic being. He explores the dark and dangerous world of Hallownest, armed with his Sword of Needles and special abilities. His unwavering determination propels him to face grotesque creatures and fearsome bosses, in the quest to unravel the ancient secrets and restore the lost glory of his kingdom.'
     },
     {
         id: 2,
         title: 'Hornet',
-        imageUrl: 'img/hornet.png',
+        imageUrl: 'img/hornet.jpg',
         body: 'Hornet, a striking character in Hollow Knight, is an agile and determined figure. She is a skilled warrior, armed with her needle and a fierce attitude. Hornet is an independent and fearless force that crosses the path of the protagonist, challenging him in intense battles.'
     },
     {
         id: 3,
         title: 'Radiance',
-        imageUrl: 'img/radiance.png',
+        imageUrl: 'img/radiance.jpg',
         body: 'Radiance, the antagonist in Hollow Knight, is a transcendent and imposing entity. It is the manifestation of the Radiant Light and represents a divine and threatening presence. Radiance is shrouded in mystery and is considered the source of the corruption that plagues Hallownest. The confrontation with her is an epic and challenging battle, requiring skill and strategy from the player. Her power-imbued presence and stunning design make her a memorable and impactful figure in the game narrative.'
     },
 ];
